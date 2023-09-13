c983d2e5a56ad1cd37cc4d88b8f915a42b9d0416
sayumikko <kuksant2708@mail.ru>
2023-09-13 21:10:27 +0300

    Added OS for tests, applied cosmetics to library names

--- project/__main__.py
+++ project/__main__.py
@@ -1,6 +1,1 @@
-def main():
-    print("Hello, world!")
-
-
-if __name__ == "__main__":
-    main()
+print("exec sources directory")
--- project/graph_module.py
+++ project/graph_module.py
@@ -1,10 +1,10 @@
-import cfpq_data as cd
-import networkx as nkx
+import cfpq_data
+import networkx as nx
 
 
 def get_graph_from_csv(name: str):
-    path = cd.download(name)
-    return cd.graph_from_csv(path)
+    path = cfpq_data.download(name)
+    return cfpq_data.graph_from_csv(path)
 
 
 def get_graph_info(name: str):
@@ -25,11 +25,11 @@ def get_graph_info(name: str):
     return info
 
 
-def save_graph_in_file(filename: str, graph: nkx.Graph):
-    pydot_graph = nkx.drawing.nx_pydot.to_pydot(graph)
+def save_graph_in_file(filename: str, graph: nx.Graph):
+    pydot_graph = nx.drawing.nx_pydot.to_pydot(graph)
     pydot_graph.write(filename)
     return
 
 
 def make_two_cycle_graph(frst_nodes: int, scnd_nodes: int, labels: tuple):
-    return cd.labeled_two_cycles_graph(frst_nodes, scnd_nodes, labels=labels)
+    return cfpq_data.labeled_two_cycles_graph(frst_nodes, scnd_nodes, labels=labels)
